fc1d05d44407b8860afcc6416d42ba9d07a7f862
jigea <igea.javier@gmail.com>
2017-06-18 13:13:15 +0100

    added GPL license

--- R/lifecycle_cvalue_habit_height_analysis.R
+++ R/lifecycle_cvalue_habit_height_analysis.R
@@ -1,3 +1,18 @@
+#################################################################################
+# (c) Javier Igea
+# igea.javier@gmail.com
+# Last modified: 18/06/2017
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#################################################################################
 library(ape)
 library(Taxonstand)
 library(taxonlookup)
